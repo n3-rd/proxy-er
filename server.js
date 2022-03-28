c953d375ddf7416b9094ae1374859b8b205e1e49
n3-rd <godwinjdayo@gmail.com>
2022-03-28 03:16:38 -0700

    formatted code & main javascript file corrected to server.js

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
-const express = require('express');
-const cors = require('cors');
-const fetch = require('node-fetch');
+const express = require("express");
+const cors = require("cors");
+const fetch = require("node-fetch");
 const app = express();
 
-app.use(cors({
-    origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get('/', (req, res)=>{
-    res.send('hello')
-    console.log('app running')
-})
+app.get("/", (req, res) => {
+  res.send("hello");
+  console.log("app running");
+});
 
-app.get('/useproxy', (req, res)=>{
-    console.log(req.query.link)
-    fetch(req.query.link)
-    .then(res => res.json())
-    .then(data => {console.log(data)
-        res.send(data)
-    
-    })
+app.get("/useproxy", (req, res) => {
+  console.log(req.query.link);
+  fetch(req.query.link)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    });
+});
 
-
-  
-})
- 
 app.listen(process.env.PORT || 8000);
